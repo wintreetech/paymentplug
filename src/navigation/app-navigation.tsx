4c92c46ea6c5f64deae7a3445aa9f89d71af397e
Shubham Makwana <shubhamwintree@gmail.com>
2025-06-18 12:50:35 +0530

    remove the product and solution direct link

--- src/navigation/app-navigation.tsx
+++ src/navigation/app-navigation.tsx
@@ -3,14 +3,11 @@ import ScrollToTop from "../components/scroll-to-top";
 
 import HomePageFour from "../pages/home-4";
 
-import ServiceV1Page from "../pages/service-v1";
-
 import AboutUsPage from "../pages/about-us";
 import ContactPage from "../pages/contact";
 
 import NotFoundPage from "../pages/not-found";
 
-import ProjectV3Page from "../pages/project-v3";
 import RiskTracker from "../pages/risk-tracker";
 import Wallet from "../pages/wallet";
 import BoardingManger from "../pages/boarding-manager";
@@ -18,28 +15,28 @@ import Invoizr from "../pages/invoizr";
 import WhiteLabel from "../pages/white-label";
 
 export default function AppNavigation() {
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePageFour />} />
+	return (
+		<BrowserRouter>
+			<ScrollToTop />
+			<Routes>
+				<Route path="/" element={<HomePageFour />} />
 
-        {/* Products */}
-        <Route path="/products" element={<ServiceV1Page />} />
-        <Route path="/products/risk-tracker" element={<RiskTracker />} />
-        <Route path="/products/wallet" element={<Wallet />} />
-        <Route path="/products/boarding-manager" element={<BoardingManger />} />
-        <Route path="/products/invoizr" element={<Invoizr />} />
+				{/* Products */}
+				{/* <Route path="/products" element={<ServiceV1Page />} /> */}
+				<Route path="/products/risk-tracker" element={<RiskTracker />} />
+				<Route path="/products/wallet" element={<Wallet />} />
+				<Route path="/products/boarding-manager" element={<BoardingManger />} />
+				<Route path="/products/invoizr" element={<Invoizr />} />
 
-        {/* Solutions */}
-        <Route path="/solutions" element={<ProjectV3Page />} />
-        <Route path="/solutions/white-label" element={<WhiteLabel />} />
+				{/* Solutions */}
+				{/* <Route path="/solutions" element={<ProjectV3Page />} /> */}
+				<Route path="/solutions/white-label" element={<WhiteLabel />} />
 
-        <Route path="/about-us" element={<AboutUsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+				<Route path="/about-us" element={<AboutUsPage />} />
+				<Route path="/contact" element={<ContactPage />} />
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
