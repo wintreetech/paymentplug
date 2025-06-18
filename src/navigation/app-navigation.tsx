b84d6c247dc233784451279cb893dc21e4be182c
Rohan-Panchal-Wintree <rohanpanchal@wintreetech.com>
2025-06-18 16:39:37 +0530

    changed the title to the paymentplug title

--- src/navigation/app-navigation.tsx
+++ src/navigation/app-navigation.tsx
@@ -15,28 +15,28 @@ import Invoizr from "../pages/invoizr";
 import WhiteLabel from "../pages/white-label";
 
 export default function AppNavigation() {
-	return (
-		<BrowserRouter>
-			<ScrollToTop />
-			<Routes>
-				<Route path="/" element={<HomePageFour />} />
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePageFour />} />
 
-				{/* Products */}
-				{/* <Route path="/products" element={<ServiceV1Page />} /> */}
-				<Route path="/products/risk-tracker" element={<RiskTracker />} />
-				<Route path="/products/wallet" element={<Wallet />} />
-				<Route path="/products/boarding-manager" element={<BoardingManger />} />
-				<Route path="/products/invoizr" element={<Invoizr />} />
+        {/* Products */}
+        {/* <Route path="/products" element={<ServiceV1Page />} /> */}
+        <Route path="/products/risk-tracker" element={<RiskTracker />} />
+        <Route path="/products/wallet" element={<Wallet />} />
+        <Route path="/products/boarding-manager" element={<BoardingManger />} />
+        <Route path="/products/invoizr" element={<Invoizr />} />
 
-				{/* Solutions */}
-				{/* <Route path="/solutions" element={<ProjectV3Page />} /> */}
-				<Route path="/solutions/white-label" element={<WhiteLabel />} />
+        {/* Solutions */}
+        {/* <Route path="/solutions" element={<ProjectV3Page />} /> */}
+        <Route path="/solutions/white-label" element={<WhiteLabel />} />
 
-				<Route path="/about-us" element={<AboutUsPage />} />
-				<Route path="/contact" element={<ContactPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
 
-				<Route path="*" element={<NotFoundPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
