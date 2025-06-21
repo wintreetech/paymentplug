d9b4ab58d4f4df7d52c71defb7b96bd1e02c30f4
Shubham Makwana <shubhamwintree@gmail.com>
2025-06-21 13:16:51 +0530

    added proper footer on every products page

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -10,71 +10,68 @@ import HeaderFour from "../layout/header/header-four";
 import NewsletterBannerTwo from "../components/newsletter/newsletter-banner-2";
 import FooterFour from "../layout/footer/footer-four";
 import BlockFeatureFive from "../components/block-feature/block-feature-five";
+import FancyBannerTwo from "../components/fancy-banner/fancy-banner-two";
 
 export default function AboutUsPage() {
-  return (
-    <Wrapper>
-      {/* seo title */}
-      <SEOCom title="About us" />
-      {/* seo title */}
+	return (
+		<Wrapper>
+			{/* seo title */}
+			<SEOCom title="About us" />
+			{/* seo title */}
 
-      <div className="main-page-wrapper">
-        {/* header start */}
-        <HeaderFour />
-        {/* header end */}
-        <main>
-          {/* breadcrumb start */}
-          <BreadcrumbOne
-            title="Trusted by Global Leaders"
-            subtitle="Powering smarter payment growth through expertise, innovation & trust."
-            page="About Us"
-            bg_img="/assets/images/media/about-us-bg.png"
-            shape="/assets/images/shape/shape-about.svg"
-            style_2={false}
-          />
-          {/* breadcrumb end */}
+			<div className="main-page-wrapper">
+				{/* header start */}
+				<HeaderFour />
+				{/* header end */}
+				<main>
+					{/* breadcrumb start */}
+					<BreadcrumbOne
+						title="Trusted by Global Leaders"
+						subtitle="Powering smarter payment growth through expertise, innovation & trust."
+						page="About Us"
+						bg_img="/assets/images/media/about-us-bg.png"
+						shape="/assets/images/shape/shape-about.svg"
+						style_2={false}
+					/>
+					{/* breadcrumb end */}
 
-          {/* text feature one area start */}
-          <TextFeatureOne style_2={true} />
-          {/* text feature one area end */}
+					{/* text feature one area start */}
+					<TextFeatureOne style_2={true} />
+					{/* text feature one area end */}
 
-          {/* block feature five start */}
-          <BlockFeatureFive />
-          {/* block feature five end */}
+					{/* block feature five start */}
+					<BlockFeatureFive />
+					{/* block feature five end */}
 
-          {/* text feature one area start */}
-          <TextFeatureThree style_2={true} />
-          {/* text feature one area end */}
+					{/* text feature one area start */}
+					<TextFeatureThree style_2={true} />
+					{/* text feature one area end */}
 
-          {/* feedback one start */}
-          <FeedbackOne cls="top-border pt-80 pb-80" />
-          {/* feedback one end */}
+					{/* feedback one start */}
+					<FeedbackOne cls="top-border pt-80 pb-80" />
+					{/* feedback one end */}
 
-          {/* partner logo start */}
-          <div className="partner-logo-one pt-80 lg-pt-40 pb-80 lg-pb-40">
-            <div className="container">
-              <PartnersSliderOne />
-            </div>
-          </div>
-          {/* partner logo end */}
+					{/* fancy banner two start */}
+					<FancyBannerTwo />
+					{/* fancy banner two end */}
 
-          {/* blogs start */}
-          <HomeFourBlogs cls="mt-180 lg-mt-80 mb-180 lg-mb-80" />
-          {/* blogs end */}
-        </main>
+					{/* blog item start */}
+					<HomeFourBlogs />
+					{/* blog item end */}
+				</main>
 
-        {/* footer area start */}
-        <div className="footer-large-wrapper position-relative">
-          {/* newsletter area start */}
-          <NewsletterBannerTwo />
-          {/* newsletter area end */}
+				{/* footer area start */}
+				<div className="footer-large-wrapper position-relative">
+					{/* newsletter area start */}
+					<NewsletterBannerTwo />
+					{/* newsletter area end */}
 
-          {/* footer start */}
-          <FooterFour />
-          {/* footer end */}
-        </div>
-        {/* footer area end */}
-      </div>
-    </Wrapper>
-  );
+					{/* footer start */}
+					<FooterFour />
+					{/* footer end */}
+				</div>
+				{/* footer area end */}
+			</div>
+		</Wrapper>
+	);
 }
--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -2,57 +2,58 @@ import Wrapper from "../layout/wrapper";
 import SEOCom from "../components/seo";
 import HeaderTwo from "../layout/header/header-two";
 import BreadcrumbOne from "../components/breadcrumb/breadcrumb-one";
-import FancyBannerThree from "../components/fancy-banner/fancy-banner-three";
 import WalletServiceDetails from "../components/services/wallet-service-details";
 import NewsletterBannerTwo from "../components/newsletter/newsletter-banner-2";
 import FooterFour from "../layout/footer/footer-four";
+import FancyBannerTwo from "../components/fancy-banner/fancy-banner-two";
+import HomeFourBlogs from "../components/blogs/home-4-blogs";
 
 export default function Wallet() {
-  return (
-    <Wrapper>
-      {/* seo title */}
-      <SEOCom title="Wallet" />
-      {/* seo title */}
+	return (
+		<Wrapper>
+			{/* seo title */}
+			<SEOCom title="Wallet" />
+			{/* seo title */}
 
-      <div className="main-page-wrapper">
-        {/* header start */}
-        <HeaderTwo />
-        {/* header end */}
-        <main>
-          {/* breadcrumb start */}
-          <BreadcrumbOne
-            title="Wallet"
-            subtitle="Enable real-time, secure transactions that build trust."
-            page="Wallet"
-            shape="/assets/images/shape/shape-wallet.svg"
-            bg_img="/assets/images/media/wallet-bg.png"
-            style_2={true}
-            cls="me-xl-4"
-          />
-          {/* breadcrumb end */}
+			<div className="main-page-wrapper">
+				{/* header start */}
+				<HeaderTwo />
+				{/* header end */}
+				<main>
+					{/* breadcrumb start */}
+					<BreadcrumbOne
+						title="Wallet"
+						subtitle="Enable real-time, secure transactions that build trust."
+						page="Wallet"
+						shape="/assets/images/shape/shape-wallet.svg"
+						bg_img="/assets/images/media/wallet-bg.png"
+						style_2={true}
+						cls="me-xl-4"
+					/>
+					{/* breadcrumb end */}
 
-          {/* service details area start */}
-          <WalletServiceDetails />
-          {/* service details area end */}
+					{/* service details area start */}
+					<WalletServiceDetails />
+					{/* service details area end */}
 
-          {/* fancy banner three start */}
-          <FancyBannerThree />
-          {/* fancy banner three end */}
+					{/* fancy banner two start */}
+					<FancyBannerTwo />
+					{/* fancy banner two end */}
 
-          {/* news letter start */}
-          {/* <NewsletterBanner /> */}
-          {/* news letter end */}
-        </main>
+					{/* blog item start */}
+					<HomeFourBlogs />
+					{/* blog item end */}
+				</main>
 
-        {/* footer start */}
-        {/* <FooterThree style_2={true} /> */}
+				{/* footer start */}
+				{/* <FooterThree style_2={true} /> */}
 
-        <div className="footer-large-wrapper position-relative">
-          <NewsletterBannerTwo />
-          <FooterFour />
-        </div>
-        {/* footer end */}
-      </div>
-    </Wrapper>
-  );
+				<div className="footer-large-wrapper position-relative">
+					<NewsletterBannerTwo />
+					<FooterFour />
+				</div>
+				{/* footer end */}
+			</div>
+		</Wrapper>
+	);
 }
